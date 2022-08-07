a158218bb509a651bb83bebb1feaed818f432c86
crpier42 <crpier42@gmail.com>
2022-08-07 22:48:18 +0300

    rework the based_vim keymap on dactyl a bit to make typing faster

---
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/based_vim/config.h
@@ -0,0 +1,88 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Use I2C or Serial, not both */
+#define USE_SERIAL
+// #define USE_I2C
+
+/* Select hand configuration */
+
+//#define MASTER_LEFT
+//#define MASTER_RIGHT
+#define EE_HANDS
+
+/* Encoder support */
+#define ENCODERS_PAD_A { F5 }
+#define ENCODERS_PAD_B { F4 }
+#define ENCODERS_PAD_A_RIGHT { F4 }
+#define ENCODERS_PAD_B_RIGHT { F5 }
+#define ENCODER_RESOLUTION 2
+//#define ENCODER_DIRECTION_FLIP
+
+// Underglow
+#undef RGB_DI_PIN
+#define RGB_DI_PIN D1
+#undef RGBLED_NUM
+#define RGBLED_NUM 18    // Number of LEDs
+#define RGBLED_SPLIT { 9, 9 }
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LIMIT_VAL 150
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+#define RGBLIGHT_LAYERS
+
+// Mouse settings
+// #undef MOUSEKEY_INTERVAL
+// #define MOUSEKEY_INTERVAL 23
+// #undef MOUSEKEY_MAX_SPEED
+// #define MOUSEKEY_MAX_SPEED 12
+// #undef MOUSEKEY_TIME_TO_MAX
+// jdefine MOUSEKEY_TIME_TO_MAX 29
+
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 16
+// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
+// give it more time to accelerate to max speed to retain precise control over short distances.
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 40
+// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY 100
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 12
+
+// Tapping settings
+// #define HOLD_ON_OTHER_KEY_PRESS
+#define IGNORE_MOD_TAP_INTERRUPT
+#define LEADER_NO_TIMEOUT
+#define LEADER_TIMEOUT 300
+#define TAPPING_TERM 130
+#define UNICODE_SELECTED_MODES UC_MAC
+
+
+// Space saving settings
+#define LAYER_STATE_8BIT
+#define USER_PRINT
+#undef LOCKING_RESYNC_ENABLE
+#undef LOCKING_SUPPORT_ENABLE
+#undef PERMISSIVE_HOLD
---
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/based_vim/keymap.c
@@ -0,0 +1,256 @@
+#include QMK_KEYBOARD_H
+
+
+#define _BASE 0
+#define _SYM2 1
+#define _SYM1 2
+#define _NAV 3
+#define _UTIL 4
+#define _UNICODE 5
+
+// FILLERS
+#define ___ KC_TRANSPARENT
+
+// LAYER KEYS
+#define SYM2  OSL(_SYM2)
+#define SYM1  OSL(_SYM1)
+#define NAV   OSL(_NAV)
+#define UTIL  OSL(_UTIL)
+#define UNICODE  OSL(_UNICODE)
+
+// LEFT HOME ROW MODS
+#define HOME_A LT(_NAV, KC_A)
+#define HOME_S LCTL_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LALT_T(KC_F)
+
+// RIGHT HOME ROW MODS
+#define HOME_SCLN LT(_NAV, KC_SCLN)
+#define HOME_L LCTL_T(KC_L)
+#define HOME_K LGUI_T(KC_K)
+#define HOME_J LALT_T(KC_J)
+
+// Other dual function keys
+#define SHIFT_ENTER LSFT_T(KC_ENTER)
+#define SHIFT_SPACE LSFT_T(KC_SPACE)
+
+// App specific keys
+#define TMUX_INNER LCTL(KC_S)
+#define TMUX_OUTER LCTL(KC_A)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* BASE 
+ * +-----------------------------------------+                             +-----------------------------------------+
+ * |      |   Q  |   W  |   E  |   R  |   T  |                             |   Y  |   U  |   I  |   O  |   P  |      |
+ * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+ * |      | A/L3 |S/CTRL|D/SUPE|F/ALT |   G  |                             |   H  | J/ALT|K/SUPE|L/CTRL| ;/L3 |      |
+ * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+ * |      |   Z  |   X  |   C  |   V  |   B  |                             |   N  |   M  |   ,  |   .  |   /  |      |
+ * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
+ *               |      |      |                                                         |      |      |
+ *               +-------------+--------------------+               +------+-------------+-------------+
+ *                             |  L1  |SHIFT |      |               |      | SHIFT|  L2  |
+ *                             |------+------|------|               |------|------+------|
+ *                                    | SPC  |      |               |      | ENTER|      
+ *                                    +-------------+               +-------------+      
+ */
+[_BASE] = LAYOUT( \
+    UNICODE, KC_Q,   KC_W,   HYPR_T(KC_E),   KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    UNICODE,   \
+    KC_LSFT, HOME_A, HOME_S, HOME_D, HOME_F, KC_G,           KC_H,   HOME_J, HOME_K,  HOME_L, HOME_SCLN, KC_LSFT, \
+    KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,           KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_LSFT,   \
+                    ___, ___,                                        ___, ___,        \
+                                    SYM1, ___,                          ___, SYM2,                                     \
+                                    SHIFT_SPACE, UTIL,                      UTIL,  SHIFT_ENTER,                                      \
+                                    KC_LEAD, RESET,                           RESET, KC_LEAD                                       \
+),
+
+/* L1 
+ * +-----------------------------------------+                             +-----------------------------------------+
+ * |      |      |      | INS  |PRTSCR|      |                             |      |   7  |   8  |   9  |   :  |      |
+ * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+ * |      | CAPS | BSPC | TAB  | ESC  |      |                             |      |   4  |   5  |   6  |   ,         |
+ * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                             |      |   1  |   2  |   3  |   .  |      |
+ * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
+ *               |      |      |                                                         |   0  |      |
+ *               +-------------+--------------------+               +------+-------------+-------------+
+ *                             |      |      |      |               |      |  L4  |      |
+ *                             |------+------|------|               |------|------+------|
+ *                                    |      |      |               |      |      |      
+ *                                    +-------------+               +-------------+      
+ */
+[_SYM1] = LAYOUT(
+    ___,  ___, ___,  KC_INS, KC_PSCREEN,   ___,              ___, KC_7, KC_8, KC_9, KC_COLON, ___, \
+    ___,  KC_CAPS, KC_BSPACE, KC_TAB, KC_ESC, ___,           ___, KC_4, KC_5, KC_6, KC_COMMA, ___, \
+    ___, ___, ___, ___, ___, ___,                            ___, KC_1, KC_2, KC_3, KC_DOT, ___, \
+                   ___, ___,                                      ___, ___,  \
+                                 ___, ___,     ___, KC_0,                                      \
+                                 ___, ___,      ___, ___,                                      \
+                                 ___, ___,     ___,   ___                                       \
+),
+
+/* L2 
+ * +------+----------------------------------+                             +-----------------------------------------+
+ * |      |  |   | &    | ^    | $    | !    |                             |      |   {  |   }  |      |      |      |
+ * |------|------+------+------+------+------+                             |------+------+------+------+------+------|
+ * |      |  =   | +    | _    | -    | @    |                             |      |   [  |   ]  | BSPC |   %         |
+ * |------|------+------+------+------+------+                             |------+------+------+------+------+------|
+ * |      |  ~   | `    | '    | "    | #    |                             |      |   (  |   )  |   *  |   \  |      |
+ * +------+------+------+------+------+-------                             +-------------+------+------+------+------+
+ *               |      |      |                                                         |      |      |
+ *               +-------------+--------------------+               +------+-------------+-------------+
+ *                             |             |      |               |      |             |
+ *                             |------+------|------|               |------|------+------|
+ *                                    |      |      |               |      |      |      
+ *                                    +-------------+               +-------------+      
+ */
+[_SYM2] = LAYOUT(
+    ___,  KC_PIPE, KC_AMPERSAND, KC_CIRCUMFLEX, KC_DOLLAR, KC_EXCLAIM,             ___, KC_LEFT_CURLY_BRACE,    KC_RIGHT_CURLY_BRACE,    ___, ___, ___, \
+    ___, KC_EQUAL, KC_PLUS, KC_UNDERSCORE, KC_MINUS, KC_AT,                        ___, KC_LBRACKET, KC_RBRACKET, KC_BSPC, KC_PERCENT, ___, \
+    ___, KC_TILDE,  KC_GRAVE,   KC_QUOTE,   KC_DOUBLE_QUOTE,  KC_HASH,             ___, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_ASTERISK, KC_BSLASH, ___, \
+                      ___, ___,                                                   ___,  ___,                    \
+                                        ___, ___,                                   ___, ___,                                          \
+                                        ___, ___,                                   ___,  ___,                                      \
+                                        ___, ___,                                     ___, ___                                        \
+
+),
+
+/* L3 
+ * +------+----------------------------------+                             +-----------------------------------------+
+ * |      |      |LCLICK| MUP  |RCLICK|      |                             | HOME | PGDN | PGUP |  END |      |      |
+ * |------|------+------+------+------+------+                             |------+------+------+------+------+------|
+ * |      |      |MLEFT |MDOWN |MRIGHT|      |                             | LEFT | DOWN |  UP  | RIGHT|      |      |
+ * |------|------+------+------+------+------+                             |------+------+------+------+------+------|
+ * |      |      |      |WHLUP |WHLDN |      |                             |      | ACC0 | ACC1 | ACC2 |      |      |
+ * +------+------+------+------+------+-------                             +-------------+------+------+------+------+
+ *               |      | DEL  |                                                         | INS  |      |
+ *               +-------------+--------------------+               +------+-------------+-------------+
+ *                             |      |      |      |               |      |      |      |
+ *                             |------+------|------|               |------|------+------|
+ *                                    |      |      |               |      |      |      
+ *                                    +-------------+               +-------------+      
+ */
+[_NAV] = LAYOUT(
+    ___, ___, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, ___,                           KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, ___, ___, \
+    ___, ___, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, RGB_VAI,                        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, ___, ___, \
+    ___, KC_MS_WH_LEFT, KC_MS_WH_UP ,KC_MS_WH_DOWN, KC_MS_WH_RIGHT, ___,           ___, KC_MS_ACCEL0, KC_MS_ACCEL1, KC_MS_ACCEL2, ___, ___, \
+                      ___, ___,                                          KC_INS, ___,                            \
+                              KC_DELETE, ___,                                           ___, ___,                                            \
+                              ___, ___,                                           ___, ___,                                            \
+                              ___, ___,                                           ___, ___                                             \
+),
+/* L4 
+ * +------+----------------------------------+                             +-----------------------------------------+
+ * |      |POWER |BRGHDN|BRGHDN|      |      |                             |      |  F7  |  F8  |  F9  |  F10 |      |
+ * |------|------+------+------+------+------+                             |------+------+------+------+------+------|
+ * |      |SLEEP |VOLDN |VOLUP | MUTE |      |                             |      |  F4  |  F5  |  F6  |  F11 |      |
+ * |------|------+------+------+------+------+                             |------+------+------+------+------+------|
+ * |      | WAKE | PREV | PLAY | NEXT |      |                             |      |  F1  |  F2  |  F3  |  F12 |      |
+ * +------+------+------+------+------+-------                             +-------------+------+------+------+------+
+ *               |      |      |                                                         |      |      |
+ *               +-------------+--------------------+               +------+-------------+-------------+
+ *                             |             |      |               |      |             |
+ *                             |------+------|------|               |------|------+------|
+ *                                    |      |      |               |      |      |      
+ *                                    +-------------+               +-------------+      
+ */
+[_UTIL] = LAYOUT(
+    ___, KC_KB_POWER, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, ___, ___,           ___, KC_F7, KC_F8, KC_F9, KC_F10, ___, \
+    ___, KC_SYSTEM_SLEEP, KC_VOLD, KC_VOLU, KC_MUTE, RGB_M_P,                                 ___, KC_F4, KC_F5, KC_F6, KC_F11, ___, \
+    ___, KC_WAKE, KC_MPRV, KC_MPLY, KC_MNXT, RGB_M_B,                                ___, KC_F1, KC_F2, KC_F3, KC_F12, ___, \
+                      RGB_HUD, RGB_HUI,                                                       RGB_TOG, RGB_MODE_FORWARD,                            \
+                                        ___, ___,                            ___, ___,                                            \
+                                        RGB_SAD, RGB_SAI,                            ___, ___,                                            \
+                                        RGB_VAD, RGB_VAI,                            ___, ___                                             \
+),
+[_UNICODE] = LAYOUT(
+    ___, UC(0x0103), ___, ___, ___, UC(0x021B),                    ___, ___, UC(0x00EE), ___, ___, ___, \
+    ___, UC(0x00E2), UC(0x0219), ___, ___, ___,           ___, ___, ___, ___, ___, ___, \
+    ___, ___, ___, ___, ___, ___,                    ___, ___, ___, ___, ___, ___, \
+                      ___, ___,                      ___, ___,                    \
+                      ___, ___,                      ___, ___,                   \
+                      ___, ___,                      ___, ___,                  \
+                      ___, ___,                      ___, ___                  \
+) 
+};
+
+LEADER_EXTERNS();
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+
+    SEQ_ONE_KEY(KC_S) {
+      send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
+    }
+    SEQ_ONE_KEY(KC_C) {
+      send_unicode_hex_string("274C");
+    }
+    SEQ_ONE_KEY(KC_A) {
+      send_unicode_hex_string("256E 0028 256F 0020 005F 2570 0020 0029 256D");
+    }
+    SEQ_ONE_KEY(KC_T) {
+      send_unicode_hex_string("2705");
+    }
+    SEQ_ONE_KEY(KC_D) {
+      send_unicode_hex_string("033F 033F 0020 033F 033F 0020 033F 033F 0020 033F 0027 033F 0027 005C 0335 0347 033F 033F 005C 0437 003D 0020 0028 0020 2580 0020 035C 035E 0296 2580 0029 0020 003D 03B5 002F 0335 0347 033F 033F 002F 2019 033F 2019 033F 0020 033F 0020 033F 033F 0020 033F 033F 0020 033F 033F");
+    }
+    SEQ_ONE_KEY(KC_Z) {
+      send_unicode_hex_string("256E 0028 256F 002D 2570 201D 0029 256D");
+    }
+  }
+}
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+/* layer_state_t layer_state_set_user(layer_state_t state) { */
+/*     switch (get_highest_layer(state)) { */
+/*     case _SYM2: */
+/*         rgblight_setrgb (0x30,  0x00, 0x00); */
+/*         break; */
+/*     case _SYM1: */
+/*         rgblight_setrgb (0x00,  0x30, 0x00); */
+/*         break; */
+/*     case _NAV: */
+/*         rgblight_setrgb (0x30,  0x00, 0x30); */
+/*         break; */
+/*     case _UTIL: */
+/*         rgblight_setrgb (0x00,  0x00, 0x30); */
+/*         break; */
+/*     default: */
+/*         rgblight_setrgb (0x00,  0x30, 0x30); */
+/*         break; */
+/*     } */
+/* return state; */
+/* } */
+
+
+// #ifdef HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+// bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//         case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+//             // Immediately select the hold action when another key is pressed.
+//             return true;
+//         default:
+//             // Do not select the hold action when another key is pressed.
+//             return false;
+//     }
+// }
+// #endif
+
+// #ifdef TAPPING_TERM_PER_KEY
+// uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//         // case HOME_O:
+//         //     return TAPPING_TERM + 30;
+//         case SHIFT_SPACE:
+//             // Very low tapping term to make sure I don't hit Enter accidentally.
+//             return TAPPING_TERM - 45;
+//         default:
+//             return TAPPING_TERM;
+//     }
+// };
+// #endif
