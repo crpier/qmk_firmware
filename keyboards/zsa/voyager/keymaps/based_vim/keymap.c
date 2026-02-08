36965cf93e19341f05be97629b04ee82eaaf2913
crpier42 <crpier42@gmail.com>
2026-02-08 02:20:52 +0200

    use shift+space and shift+enter in typing layer

--- keyboards/zsa/voyager/keymaps/based_vim/keymap.c
+++ keyboards/zsa/voyager/keymaps/based_vim/keymap.c
@@ -98,14 +98,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+-------+                     |-------+-------+-------+-------+-------+-------|
  * | CTRL  |   Z   |   X   |   C   |   V   |   B   |                     |   N   |   M   |   ,   |   .   |   /   | CTRL  |
  * +-------+-------+-------+-------+-------+-------+-------+     +-------+-------+-------+-------+-------+-------+-------+
- *                                         | SYM1  | SPACE |     | ENTER | SYM2  |
+ *                                         | SYM1  |SFT/SCP|     |SFT/ENT| SYM2  |
  *                                         +-------+-------+     +-------+-------+ */
   [TYPING] = LAYOUT_voyager(
     KC_LGUI,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                      KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,DF_BASE,
     KC_LALT,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                      KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,_______,
     KC_LSFT,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                      KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_LSFT,
     KC_LCTL,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                      KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_LCTL,
-                                            OSLSYM1,KC_SPC ,      KC_ENT ,OSLSYM2
+                                            OSLSYM1,SFT_SPC,      SFT_ENT,OSLSYM2
   ),
 /* +-----------------------------------------------+                     +-----------------------------------------------+
  * |       |       |       |       |       |       |                     |       |       |       |       |       |       |
